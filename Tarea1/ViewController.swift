440be9862e4fdd7b1ffea6772c6a5df6c3805880
jonathanavilap1 <40183554+jonathanavilap1@users.noreply.github.com>
2021-12-20 12:03:36 -0600

    Initial Commit

---
+++ Tarea1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tarea1
+//
+//  Created by Jonathan Avila on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
